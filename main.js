9ee7bd313fca648d0c0a75df60de38bbd5612063
Burdusa Daniel <danielburdusa26@gmail.com>
2023-06-29 16:02:01 +0200

    printing in html

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-let nomeUtente = prompt('qual\'è il tuo nome?');
+let  nameUser = prompt("qual\'è il tuo nome?");
 let cognomeUtente = prompt('qual\'è il tuo cognome?');
-let colerePreferito = prompt('qual\'è il tuo colore preferito?');
+let colorePreferito = prompt('qual\'è il tuo colore preferito?');
 let primoNumero = parseInt(prompt('dammi un numero'));
 let secondoNumero = parseInt(prompt('dammi un secondo numero'));
 let divisioneNumeri = primoNumero / secondoNumero;
-let password = nomeUtente + cognomeUtente + colerePreferito + divisioneNumeri
+let password =  nameUser + cognomeUtente + colorePreferito + divisioneNumeri;
 console.log(password);
+let bigliettinoDaVisita = `---------------biglietto-----------
+nome: ${nameUser}
+`
+console.log(bigliettinoDaVisita);
+
+document.getElementById("yourName").innerHTML = nameUser;
+/*document.getElementById("yourSurname").innerHTML = cognomeUtente;
+document.getElementById("fav_color").innerHTML = colorePreferito;
+document.getElementById("generated_number").innerHTML = divisioneNumeri;*/
