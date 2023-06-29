940cba2dffc2bdb279b81bd67d36ba5dca7b6dc9
Burdusa Daniel <danielburdusa26@gmail.com>
2023-06-29 17:53:53 +0200

    final product

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let  nameUser = prompt("qual\'è il tuo nome?");
+let nameUser = prompt('qual\'è il tuo nome?');
 let cognomeUtente = prompt('qual\'è il tuo cognome?');
 let colorePreferito = prompt('qual\'è il tuo colore preferito?');
 let primoNumero = parseInt(prompt('dammi un numero'));
 let secondoNumero = parseInt(prompt('dammi un secondo numero'));
 let divisioneNumeri = primoNumero / secondoNumero;
-let password =  nameUser + cognomeUtente + colorePreferito + divisioneNumeri;
+let password = nameUser + cognomeUtente + colorePreferito + divisioneNumeri;
 console.log(password);
 let bigliettinoDaVisita = `---------------biglietto-----------
 nome: ${nameUser}
@@ -12,6 +12,7 @@ nome: ${nameUser}
 console.log(bigliettinoDaVisita);
 
 document.getElementById("yourName").innerHTML = nameUser;
-/*document.getElementById("yourSurname").innerHTML = cognomeUtente;
+document.getElementById("yourSurname").innerHTML = cognomeUtente;
 document.getElementById("fav_color").innerHTML = colorePreferito;
-document.getElementById("generated_number").innerHTML = divisioneNumeri;*/
+document.getElementById("generated_number").innerHTML = divisioneNumeri;
+document.getElementById("yourPassword").innerHTML = password;
